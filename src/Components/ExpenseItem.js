76acc8c6bff6c865d8045128dbca12b3bc5b810e
IrfanCatovic <catovicc84@gmail.com>
2023-09-17 14:54:56 +0200

    Prvi cas nedelja

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,13 +1,21 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  {
+    /* u zagradi ('Prvo se navodi koji jezik, {objekat gde konfigurises kako bi trebalo da se prikaze u kom formatu }) */
+  }
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        {/* u zagradi ('Prvo se navodi koji jezik, {objekat gde konfigurises kako bi trebalo da se prikaze u kom formatu }) */}
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
