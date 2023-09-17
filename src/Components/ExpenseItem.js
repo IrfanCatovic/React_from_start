a4fca0b28698337da1073eeb4d91a5f0c05b3a9e
IrfanCatovic <catovicc84@gmail.com>
2023-09-17 19:23:07 +0200

    Cas 2 nedelja

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,21 +1,11 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  {
-    /* u zagradi ('Prvo se navodi koji jezik, {objekat gde konfigurises kako bi trebalo da se prikaze u kom formatu }) */
-  }
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        {/* u zagradi ('Prvo se navodi koji jezik, {objekat gde konfigurises kako bi trebalo da se prikaze u kom formatu }) */}
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
+      {/* Ovo u zagradi ga salje dalje u componentu expenseDate  */}
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
