0deb305301568160ff702f8008d733af638447c8
IrfanCatovic <catovicc84@gmail.com>
2023-09-19 15:43:58 +0200

    multi states

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
   const titleChangeHandler = (event) => {
-    console.log(event.target.value); //ovo koristmo jer iz eventa vadimo podatke, tj event je svaki put kad ukucamo slovo i trazimo koje je to slovo tj rec
+    setEnteredTitle(event.target.value); //ovo koristmo jer iz eventa vadimo podatke, tj event je svaki put kad ukucamo slovo i trazimo koje je to slovo tj rec
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
   };
 
   return (
@@ -14,15 +26,25 @@ const ExpenseForm = () => {
           <label>Title</label>
           <input type="text" onChange={titleChangeHandler} />
         </div>
-
+ 
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min={0.01} step={0.01} />
+          <input
+            type="number"
+            min={0.01}
+            step={0.01}
+            onChange={amountChangeHandler}
+          />
         </div>
 
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019 - 01 - 01" max="2022-12-31" />
+          <input
+            type="date"
+            min="2019 - 01 - 01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
