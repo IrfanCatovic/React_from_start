8d4749b47d4f4bafdb52dd0acec6cf20e3ed1660
IrfanCatovic <catovicc84@gmail.com>
2023-09-20 04:14:04 +0200

    update state from prev

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -3,20 +3,39 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredAmount, setEnteredAmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  // const [enteredTitle, setEnteredTitle] = useState("");
+  // const [enteredAmount, setEnteredAmount] = useState("");
+  // const [enteredDate, setEnteredDate] = useState("");
+
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    EnteredAmount: "",
+    eneteredDate: "",
+  }); //Umesto ovoga iznad
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value); //ovo koristmo jer iz eventa vadimo podatke, tj event je svaki put kad ukucamo slovo i trazimo koje je to slovo tj rec
+    // setEnteredTitle(event.target.value); //ovo koristmo jer iz eventa vadimo podatke, tj event je svaki put kad ukucamo slovo i trazimo koje je to slovo tj rec
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+      //ovo uzimamo ceo objekt userInput i samo preko Title ispisemo novi uneti title
+    });
   };
 
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
+    // setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value, //objasnjenje u title
+    });
   };
 
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
+    // setEnteredDate(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value, //objasnjenje u title
+    });
   };
 
   return (
@@ -26,7 +45,7 @@ const ExpenseForm = () => {
           <label>Title</label>
           <input type="text" onChange={titleChangeHandler} />
         </div>
- 
+
         <div className="new-expense__control">
           <label>Amount</label>
           <input
