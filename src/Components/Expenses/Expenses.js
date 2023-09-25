39df613d57612d8b99859d918a4b0e16ca0a5288
IrfanCatovic <catovicc84@gmail.com>
2023-09-25 17:55:36 +0200

    first lecture mon

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -17,7 +17,17 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
+      {props.items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+      {/* .MAP je funkcija koja uzima svaki element datog niza
+           primamo svaki element iz APP i od svakog pravimo ExpenseItem componentu*
+           Novi code sa .map funkcijom koju sam napisao menja ceo code koji se nalazi ispod/}
+      {/* <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
@@ -36,7 +46,7 @@ const Expenses = (props) => {
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
-      />
+      /> */}
     </Card>
   );
 };
